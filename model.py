f1cb203d1db7a7eae57e8f024da27d17a400c43a
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-05-29 04:49:52 -0300

    Criando a entidade do usuário

---
+++ model.py
@@ -0,0 +1,9 @@
+from __init__ import db
+from sqlalchemy import Column, Text, Integer, VARCHAR
+
+class usuario (db.Model):
+    __tablename__ = "Usuarios"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=False)
+    nome = Column(Text, nullable=False, unique=True)
+    password = Column(VARCHAR(16), nullable=False, unique=False)
