46853b696438ba2eb981b7f24c844a3c7873295a
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-05-29 04:46:43 -0300

    Setup inicial

---
+++ __init__.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+load_dotenv(dotenv_path=r"Super_Secrets/MySecrets.env")
+
+app = Flask(__name__)
+app.secret_key = os.getenv("SECRET_KEY")
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///MeuDados.db"
+
+db = SQLAlchemy(app)
+
+from routes import *
+from model import *
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
