77c2a2a90fc0df9b9323140dd44f439d105567b4
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-07-20 09:41:42 -0300

    Herdando os atributos da classe pai UserMixin para a classe User

--- model.py
+++ model.py
@@ -1,9 +1,18 @@
-from __init__ import db
-from sqlalchemy import Column, Text, Integer, VARCHAR
+from extensions import db
+from sqlalchemy import Column, Text, Integer, VARCHAR, ForeignKey
+from flask_login import UserMixin
 
-class Usuario (db.Model):
-    __tablename__ = "Usuarios"
+class User(db.Model, UserMixin):
+    __tablename__ = "user"
 
     id = Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=False)
-    nome = Column(Text, nullable=False, unique=True)
+    name = Column(Text, nullable=False, unique=True)
     password = Column(VARCHAR(16), nullable=False, unique=False)
+
+class Message(db.Model):
+    __tablename__ = "message"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, unique=True, nullable=False)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    user = db.relationship("User", backref=db.backref("user", uselist=False))
+    message_text = Column(VARCHAR(200), unique=False, )
