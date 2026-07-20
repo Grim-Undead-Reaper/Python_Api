2fd34f60fff5603201e0a44ceeb7633009e33a64
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-07-20 10:10:26 -0300

    Alterando o nome do arquivo, tambem alterando suas rotas

---
+++ Constants.py
@@ -0,0 +1,10 @@
+AllRoutes = {
+"/": "Homepage",
+"/api/add/<name>": "Save and show the name in url",
+"/api/update/<oldname>/<newname>": "Update a old username",
+"/api/delete/<name>": "Delete selected user",
+"/api/search/<name>": "Search for a user",
+"/api/routes": "Show all routes and his description",
+"/auth/register":"Register user page",
+"/auth/login": "Make user login"
+}
