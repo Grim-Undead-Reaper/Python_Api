7cf2200eb738bc83eb902f07d2eef0c43834bdf5
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-07-20 07:14:59 -0300

    Criando um arquivo que tem todas as rotas da aplicação e sua descrição, pra evitar de por hard coded nas rotas

---
+++ Constantes.py
@@ -0,0 +1,9 @@
+AllRoutes = {
+"/": "Homepage",
+"/add/<name>": "Save and show the name in url",
+"/register":"Register user page",
+"/login": "Make user login",
+"/update<oldname>/<newname>": "Update a old username",
+"/delete<name>": "Delete selected user",
+"/search<name>": "Search for a user",
+"/routes": "Show all routes and his description"}
