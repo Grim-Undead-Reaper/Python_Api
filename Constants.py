792b8ab1f19d692ecbac5ee7a7bb15a7f2c41f50
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-07-26 11:14:31 -0300

    Mudando a maneira como armazeno as rotas

--- Constants.py
+++ Constants.py
@@ -1,10 +1,10 @@
-AllRoutes = {
-"/": "Homepage",
-"/api/add/<name>": "Save and show the name in url",
-"/api/update/<oldname>/<newname>": "Update a old username",
-"/api/delete/<name>": "Delete selected user",
-"/api/search/<name>": "Search for a user",
-"/api/routes": "Show all routes and his description",
-"/auth/register":"Register user page",
-"/auth/login": "Make user login"
-}
+ListOfRoutes = []
+
+ListOfRoutes.append({"url":"/", "description": "Homepage", "statusCode":[200, 400], "methodsAllowed":["GET", "POST"]})
+ListOfRoutes.append({"url":"/api/add/<name>", "description": "Save and show the name in url.", "statusCode":[200, 400], "methodsAllowed":["GET", "POST"]})
+ListOfRoutes.append({"url":"/api/update/<oldname>/<newname>", "description": "Update a old username.", "statusCode":[200, 400], "methodsAllowed":["GET", "POST"]})
+ListOfRoutes.append({"url":"/api/delete/<name>", "description": "Delete selected user.", "statusCode":[200, 400], "methodsAllowed":["GET", "POST"]})
+ListOfRoutes.append({"url":"/api/search/<name>", "description": "Search for a user.", "statusCode":[200, 400], "methodsAllowed":["GET", "POST"]})
+ListOfRoutes.append({"url":"/api/routes", "description": "how all routes and his description.", "statusCode":[200, 400], "methodsAllowed":["GET", "POST"]})
+ListOfRoutes.append({"url":"/auth/register", "description": "Register user page.", "statusCode":[200, 400], "methodsAllowed":["GET", "POST"]})
+ListOfRoutes.append({"url":"/auth/login", "description": "Make user login.", "statusCode":[200, 400], "methodsAllowed":["GET", "POST"]})
