7efd4f45411367c69e585948c3d7d4e3cced801f
PedroSantosDK <pedrogabrieldkx@gmail.com>
2026-07-26 11:05:10 -0300

    Criando um blueprint com as rotas refentes a autenticação

--- Blueprints/AuthenticationBlueprint.py
+++ Blueprints/AuthenticationBlueprint.py
@@ -2,10 +2,6 @@ from flask import Blueprint, render_template
 
 authbp = Blueprint("AuthenticationBlueprint", __name__, url_prefix="/auth")
 
-@authbp.route("/register", methods=["POST", "GET"])
-def register():
-    return render_template("AuthenticationPage.html")
-
-@authbp.route("/login", methods=["POST", "GET"])
-def login():
+@authbp.route("", methods=["POST", "GET"])
+def Auth():
     return render_template("AuthenticationPage.html")
